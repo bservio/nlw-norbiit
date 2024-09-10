4d8845f4d9356f11aad56b3cdd751add70c3ace8
Bruno Sérvio <brunoservio21@gmail.com>
2024-09-10 17:36:09 -0300

    feat: project setup and first service and route

---
+++ src/http/server.ts
@@ -0,0 +1,43 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createGoal } from '../services/create-goal'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+
+const port = 3333
+
+app
+  .listen({
+    port: port,
+  })
+  .then(() => {
+    console.log(`Http server running on PORT: ${port}`)
+  })
